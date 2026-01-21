cf601eb6147ed644c4b95626c4f2389a13a4cae6
Cikepuke1 <kozarskijaleksandr0@gmail.com>
2026-01-21 11:38:27 +0700

    добавил модели

---
+++ source/DirectoryService.solution/Domain/LocationsContext/Location.cs
@@ -0,0 +1,43 @@
+﻿using Domain.LocationsContext.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace Domain.LocationsContext
+{
+    public sealed class EntityLifeTime
+    {
+        public DateTime CreatedAt { get; }
+        public DateTime UpdatedAt { get; }
+        public bool IsActivate { get; }
+
+        private EntityLifeTime(DateTime createdAt, DateTime updatedAt, bool isActivate)
+        {
+            CreatedAt = createdAt;
+            UpdatedAt = updatedAt;
+            IsActivate = isActivate;
+        }
+
+        public static EntityLifeTime Create(DateTime createdAt, DateTime updatedAt, bool isActivate)
+        {
+            if (createdAt == DateTime.MinValue || createdAt == DateTime.MaxValue)
+                throw new ArgumentException("Некорректное значение даты создания.", nameof(createdAt));
+
+            if (updatedAt == DateTime.MinValue || updatedAt == DateTime.MaxValue)
+                throw new ArgumentException(
+                    "Некорректное значение даты обновления.",
+                    nameof(updatedAt)
+                );
+
+            if (updatedAt < createdAt)
+                throw new ArgumentException(
+                    "Дата обновления не может быть меньше даты создания.",
+                    nameof(updatedAt)
+                );
+
+            return new EntityLifeTime(createdAt, updatedAt, isActivate);
+        }
+    }
+}
