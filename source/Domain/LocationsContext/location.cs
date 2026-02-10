a864e66a657532660138c8ff777130560680c912
Cikepuke1 <kozarskijaleksandr0@gmail.com>
2026-02-10 09:21:12 +0700

    ggg

---
+++ source/Domain/LocationsContext/location.cs
@@ -0,0 +1,40 @@
+﻿using DirectoryService.Domain.Shared;
+using Domain.LocationsContext.ValueObjects;
+
+namespace Domain.Location
+{
+    public class Location
+    {
+        public LocationId Id { get; }
+        public LocationName Name { get; }
+        public LocationAddress Address { get; }
+        public IanaTimeZone TimeZone { get; set; }
+        public EntityLifeTime LifeTime { get; set; }
+
+        public Location(
+            LocationId id,
+            LocationAddress address,
+            LocationName name,
+            IanaTimeZone timeZone,
+            EntityLifeTime lifeTime
+        )
+        {
+            Id = id;
+            Address = address;
+            Name = name;
+            TimeZone = timeZone;
+            LifeTime = lifeTime;
+        }
+
+        public void ChangeIanaTimeZone(IanaTimeZone newname)
+        {
+            if (LifeTime.IsActive == false)
+            {
+                throw new InvalidOperationException("Сущность удалена");
+            }
+
+            TimeZone = newname;
+            LifeTime = LifeTime.Update();
+        }
+    }
+}
